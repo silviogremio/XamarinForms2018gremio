099ff0aa89c69af1b606a41e1bc0df41e34910d4
Silvio <m125481@ipatingamg.gov.br>
2018-11-30 16:09:44 -0200

    App 1 Concluído

---
+++ Secao05/Secao05/Secao05/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using App1_ConsultarCEP.Servico.Modelo;
+using App1_ConsultarCEP.Servico;
+
+namespace Secao05
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BOTAO.Clicked += BuscarCEP;                
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            string cep = CEP.Text.Trim();
+
+            if (isValidCEP(cep))
+            {
+
+                try
+                {
+                    //ponto mais vulnerável
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                    if(end.cep != null)
+                    {
+                        string strJson = "";
+
+                        strJson = string.Format("Endereço:@" +
+                                                        "Rua: {0}@" +
+                                                        "Bairro: {1}@" +
+                                                        "Cidade: {2}@" +
+                                                        "Estado: {3}@", end.logradouro, end.bairro, end.localidade, end.uf);
+
+                        strJson = strJson.Replace("@", System.Environment.NewLine);
+
+                        RESULTADO.Text = strJson;
+                    }
+                    else
+                    {
+                        DisplayAlert("NÃO EXISTE", "CEP informado é inexistente. CEP informado: " + cep, "OK");
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("ERRO CRÍTICO", e.Message, "ok");
+                }
+
+
+            }
+
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+
+            //verifica se possui 8 caracteres
+            if (cep.Length != 8)
+            {
+                //título da mensagem, conteúdo e nome do botão
+                DisplayAlert("ERRO", "CEP inválido! O CEP deve conter 8 caracteres","OK");
+                valido = false;
+            }
+
+            //verifica se possui apenas números
+            int novoCEP = 0;
+            if (!int.TryParse(cep, out novoCEP))
+            {
+                DisplayAlert("ERRO", "CEP inválido! O CEP deve ser composto apenas por números", "OK");
+                valido = false;
+            }
+
+
+            return valido;
+        }
+
+    }
+}
---
+++ Secao05/Secao05/Secao05/Servico/ViaCEPServico.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App1_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App1_ConsultarCEP.Servico
+{
+    public class ViaCEPServico
+    {
+		//utilizada para fazer download/obter endereço preenchido
+		private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+		public static Endereco BuscarEnderecoViaCEP(string cep)
+		{
+			string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            //método síncrono trava a tela e não faz nada em cima do plano
+            WebClient wc = new WebClient();            
+            string conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            //desserializar é pegar o JSON e converter para um objeto do tipo endereço
+            //NewtonSoftware é uma biblioteca popular para desserializar
+            //Desserializando o JSon que está na variável conteudo, definido como tipo saída Endereco
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            //JSON retorna null quando o CEP informado não existe.
+            if (end.cep == null) return null;
+
+            return end;
+            
+		}
+    }
+}
